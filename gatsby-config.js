f6ed7103573fec7033ce26db4d09040e1d572004
Surafel Yimam <surafelx@Surafels-MacBook-Pro.local>
2020-11-01 19:04:04 +0300

    Added Google Fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,16 @@ module.exports = {
           },
         ]
       },
-    }, 
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i,`
+        ],
+        display: "swap",
+      },
+    },  
   ],
 }
