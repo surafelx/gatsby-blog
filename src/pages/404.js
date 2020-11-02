07a784fbdaf7b9505dacc608433d6ba4be9799ff
Surafel Yimam <surafelx@Surafels-MacBook-Pro.local>
2020-11-02 16:33:13 +0300

    404 Page Created

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,4 +3,29 @@ import { graphql } from "gatsby"
 import { Container, Content, FeatureImage } from "../components"
 import { H1 } from "../elements"
 
-const not
+const notFound = ({data}) => {
+    const featureImage = data.imageSharp.fixed
+    return (
+        <Container>
+            <FeatureImage fixed={featureImage}/>
+                <Content>
+                    <H1 textAlign="center" margin="0 0 1rem">
+                        {" "}
+                        Uh-oh... Not Found. 
+                    </H1>
+                </Content>
+        </Container>
+    )
+}
+
+export default notFound
+
+export const notFoundQuery = graphql`
+    query NotFoundQuery {
+        imageSharp(fixed: {originalName: {eq:"sad-man.jpg"}}) {
+            fixed {
+                ...GatsbyImageSharpFixed
+            }
+        }
+    }
+`
