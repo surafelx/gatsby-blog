fd110b397ce4c6f0c1393dd60ecb262ace9cc155
Surafel Yimam <surafelx@Surafels-MacBook-Pro.local>
2020-11-02 06:53:24 +0300

    Optimized Images (OMGSVG) and Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+    FooterWrapper,
+    FooterSocialWrapper,
+    FooterSocialIcons,
+} from "../elements"
+
+export const Footer = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            facebook: file(relativePath:{eq: "facebook.svg"}) {
+                publicURL
+            }
+            linkedin: file(relativePath:{eq: "linkedin.svg"}) {
+                publicURL
+            }
+            twitter: file(relativePath:{eq: "twitter.svg"}) {
+                publicURL
+            }
+            instagram: file(relativePath:{eq: "instagram.svg"}) {
+                publicURL
+            }
+        }
+    `)
+    return <FooterWrapper>
+    <FooterSocialWrapper>
+        <FooterSocialIcons>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferer">
+                <img src={data.facebook.publicURL} alt="Facebook Logo" />
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferer">
+                <img src={data.linkedin.publicURL} alt="Linkedin Logo" />
+            </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferer">
+                <img src={data.twitter.publicURL} alt="Twitter Logo" />
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferer">
+                <img src={data.instagram.publicURL} alt="Instagram Logo" />
+            </a>
+        </FooterSocialIcons>
+        <p>
+            2020 Company. All Rights Reserved.
+        </p>
+    </FooterSocialWrapper>
+    </FooterWrapper>
+}
--- src/elements/NavElements.js
+++ src/elements/NavElements.js
@@ -5,6 +5,11 @@ export const NavWrapper = styled.nav`
     grid-row: 1 / 2;
     display: flex;
     align-items: center;
+
+    & img {
+        height: 30px;
+    }
+
         @media ${props => props.theme.breakpoints.tablet} {
         grid-column: 2 / span 6;
         }
